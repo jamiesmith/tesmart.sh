52af0a4b5d28eefee574ad40292d69668e80f7a9
Jamie Smith <jamie@jamieandamy.com>
2024-07-11 16:21:01 -0400

    also generate next and previous

--- generate-input-wrappers.sh
+++ generate-input-wrappers.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for input in $(seq 1 8)
+for input in $(seq 1 8) next previous
 do
     ln -s kvminput.sh kvminput-${input}.sh
 done
+
--- kvminput.sh
+++ kvminput.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
 command=$0
-date >> /tmp/foo
+
+export PATH=${PATH}:~/Dropbox/bin:/opt/homebrew/bin:/usr/local/bin/:~/bin/
+
+minInput=1
+maxInput=8
 
 tmp=${command%*.sh}
 input=${tmp##*-}
+cur=$(tesmart-ctl get-input | head -1 | awk '{print $4}')
 
-export PATH=${PATH}:~/Dropbox/bin:/opt/homebrew/bin:/usr/local/bin/:~/bin/
+if [ "$input" = "previous" ]
+then
+    input=$(($cur - 1))
+    [ $input -lt $minInput ] && input=$maxInput
+elif [ "$input" = "next" ]
+then
+    input=$(($cur + 1))
+    [ $input -gt $maxInput ] && input=$minInput
+fi
+
 
 tesmart-ctl s $input
 tesmart-ctl g
